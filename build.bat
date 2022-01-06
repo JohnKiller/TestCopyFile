13507960bc754408c4c6d7045f126c7535bfd94a
Gianni Angelozzi <gianni@sistema3.it>
2022-01-06 11:59:53 +0100

    scss scoped test

--- build.bat
+++ build.bat
@@ -1,10 +1,9 @@
+@echo off
 rd /s /q RazorClassLibrary1\bin
 rd /s /q RazorClassLibrary1\obj
 rd /s /q BlazorApp1\bin
 rd /s /q BlazorApp1\obj
-del RazorClassLibrary1\wwwroot\test_library.txt
 cd BlazorApp1
 dotnet publish -c Release
-cd bin\Release\net6.0\publish\wwwroot\_content\RazorClassLibrary1
-dir
+dir bin\Release\net6.0\publish\wwwroot\_content\RazorClassLibrary1
 pause
